d62e5064db4ef6c8d796fa9a6202bf2eea79e453
barron9 <barron9@yandex.com>
2018-12-16 15:49:12 +0300

    asd

---
+++ WebApplication5/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary1;
+
+namespace WebApplication5.Controllers
+{
+    public class DefaultController : ApiController
+    {
+
+  
+
+
+        [HttpGet]
+        public IEnumerable<Table_1> Get(string name) {
+            using (testEntities2 entities = new testEntities2())
+            {
+                Table_1 us = new Table_1()
+                { id=0, name=$"{name}"};
+                entities.Table_1.Add(us);
+               
+                    entities.SaveChanges();
+ 
+
+                return entities.Table_1.ToList();
+
+
+            }
+            
+           
+
+
+
+        }
+
+    }
+}
